8a2e904b4843fdc223f7b97c0c86b15a3f06faed
RiftSTW <33435468+RiftSTW@users.noreply.github.com>
2020-11-19 21:57:54 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# ShopBot
+Bot programmed in Node.JS that grabs the Fortnite item shop and puts them in a neat image and posts them on twitter!
+
+Any problems? Feel free to tell me on my discord server! https://discord.js/GvHfG33
+
+# Installation & Requirements
+
+Requires:
+
+Node.JS
+
+Node.JS can be found here: https://nodejs.org/en/ (It is recommended to download the LTS version)
+
+Installation:
+
+Install all of the required packages by doing "npm install" (without quotations)
+
+Go to app.js and go to line 26 and change all of the keys from line 26 through 29 to your Twitter API Keys.
+Go to config.json and change your sac to yours (If you don't have one change it to something like "I don't have one.")
+
+Once your API Keys are in the app.js save the js file and do the same with your sac code in config.json load start.bat and watch the bot tweet the shop!
---
+++ app.js
@@ -0,0 +1,58 @@
+//Fortnite Shop Bot developed by: RiftSTW
+
+const axios = require("axios");
+const Twitter = require("twitter");
+const Canvas = require("canvas")
+const fs = require("fs")
+const config = require("./config.json")
+const { GenerateShop } = require('./functions/shop.js')
+
+console.log("Fortnite Shop Bot developed by: RiftSTW")
+console.log(`SAC Code used: ${config.sac}`)
+console.log("Grabbing Item Shop Image...")
+
+fs.stat('./final/shop.png', async function(err, stats) {
+    if(!err) {
+    console.log("No error lol")
+    }
+    else {
+        const response = await axios({method: 'get',url: 'https://fortnite-api.com/v2/shop/br/combined?language=en'})
+        await GenerateShop(response.data.data).then(async (value) => {
+            
+        })
+    }
+
+    const client = new Twitter({
+    consumer_key: 'PUT KEY HERE',
+    consumer_secret: 'PUT KEY HERE',
+    access_token_key: 'PUT KEY HERE',
+    access_token_secret: 'PUT KEY HERE'
+  });
+
+  const data = require('fs').readFileSync("./final/shop.png");
+
+  // Make post request on media endpoint. Pass file data as media parameter
+  client.post('media/upload', {media: data}, function(error, media, response) {
+  
+    if (!error) {
+  
+      // If successful, a media object will be returned.
+      console.log(media);
+  
+      // Lets tweet it
+      var date = new Date();
+      var status = {
+        status: `#Fortnite Item Shop for Sunday, ${date} #FortniteBR #FortniteItemShop Use Support-A-Creator Code: ${config.sac} #ad`,
+        media_ids: media.media_id_string
+      }
+  
+      client.post('statuses/update', status, function(error, tweet, response) {
+        if (!error) {
+          console.log(tweet);
+          console.log("Item shop has been tweeted!")
+        }
+      });
+  
+    }
+  }); 
+  })
